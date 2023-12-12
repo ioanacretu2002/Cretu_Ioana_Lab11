55e45af552dd521174565f9da57c7dbc45f5d61e
ioanacretu2002 <ioanacretu2002@yahoo.com>
2023-12-13 01:03:25 +0200

    Laborator 11

--- Cretu_Ioana_Lab11/App.xaml.cs
+++ Cretu_Ioana_Lab11/App.xaml.cs
@@ -1,12 +1,14 @@
-﻿namespace Cretu_Ioana_Lab11
+﻿using Cretu_Ioana_Lab11.Data;
+
+namespace Cretu_Ioana_Lab11
 {
     public partial class App : Application
     {
+        public static ShoppingListDatabase Database { get; private set; }
         public App()
         {
-            InitializeComponent();
-
-            MainPage = new AppShell();
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
         }
     }
 }
